b76d6608768dc030a21505b1ac192ce0552a0015
靳学军 <xuejun.jin@sh-rfid.com>
2023-07-10 20:51:11 +0800

    feat: xiugai

--- src/component/content/index.tsx
+++ src/component/content/index.tsx
@@ -8,10 +8,11 @@
  */
 
 import React, { useState } from 'react';
-import { Space, Table,  Button, message, Upload, Tag  } from 'antd';
+import { Space, Table, Button, message, Upload, Tag, Popconfirm } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import type { ColumnsType } from 'antd/es/table';
 import type { UploadProps } from 'antd';
+import cloneDeep from 'lodash/cloneDeep'
 
 interface metaOptions {
   title: string;
@@ -33,13 +34,15 @@ interface routerOptions {
   meta: metaOptions;
 }
 
-let setTableData : React.Dispatch<React.SetStateAction<routerOptions[]>>
+let setTableData: React.Dispatch<React.SetStateAction<routerOptions[]>>
 
 type fucToTree = (router: routerOptions[], parentId: string) => routerOptions[]
 
 // const SYSTEMID = "05395ecdee02408d9ef1114185b736d4" // 系统id
 
-const toTree:fucToTree = (router, parentId) => {
+let originData: routerOptions[]
+
+const toTree: fucToTree = (router, parentId) => {
   const parentIdMap = new Map<idType, routerOptions>();
 
   router.forEach((item) => {
@@ -58,6 +61,7 @@ const toTree:fucToTree = (router, parentId) => {
 
       parent.children.push(item);
     } else {
+      item.parentId = parentId
       result.push(item);
     }
   });
@@ -65,18 +69,32 @@ const toTree:fucToTree = (router, parentId) => {
   return result.filter((item) => item.parentId === parentId);
 };
 
+const confirm = (e: React.MouseEvent<HTMLElement>, record: routerOptions) => {
+  console.log(e);
+  originData = originData.filter(item => item.path !== record.path)
+  const router = toTree(originData, originData[0].parentId)
+  setTableData(router)
+
+
+};
+
+const cancel = (e?: React.MouseEvent<HTMLElement>) => {
+  // console.log(e);
+  // message.error('Click on No');
+};
+
 const columns: ColumnsType<routerOptions> = [
   {
     title: 'title',
     dataIndex: 'meta.title',
     render(_, record) {
-        return record.meta.title
+      return record.meta.title
     },
   },
   {
     title: '是否菜单',
     dataIndex: 'meta.isMenu',
-    render(_,record) {
+    render(_, record) {
       return record.meta.isMenu === 'true' ? <Tag color="success">菜单页面</Tag> : <Tag color="error">子路由</Tag>
     }
   },
@@ -91,9 +109,19 @@ const columns: ColumnsType<routerOptions> = [
     key: 'action',
     render: (_, record) => (
       <Space size="middle">
-        <a>新增</a>
+        {record.meta.isMenu === 'true' && <a>新增</a>}
         <a>编辑</a>
-        <a>删除</a>
+        <Popconfirm
+          title="确定要删除吗？"
+          description="删除后，子菜单和路由都展示在第一层"
+          onConfirm={(e) => confirm(e!, record)}
+          onCancel={cancel}
+          okText="Yes"
+          cancelText="No"
+        >
+          <a>删除</a>
+        </Popconfirm>
+
       </Space>
     ),
   },
@@ -102,41 +130,44 @@ const columns: ColumnsType<routerOptions> = [
 const data: routerOptions[] = [];
 
 const props: UploadProps = {
-  showUploadList:false,
+  showUploadList: false,
   name: 'file',
   accept: '.json',
   beforeUpload(file) {
-    
-    const reader = new FileReader()
-    reader.onload =(e) => {
-      const router:routerOptions[] = JSON.parse(e.target?.result as string) as routerOptions[]
 
-    const result =  toTree(router,router[0].parentId)
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      originData = JSON.parse(e.target?.result as string) as routerOptions[]
+
+      const result = toTree(originData, originData[0].parentId)
       setTableData(result)
-      
+
     }
     reader.readAsText(file)
-    
+
   },
   customRequest() {
     return
   }
 }
 
-const Upfile = ()=> {
+const Upfile = () => {
+  console.log('Upfile-render');
   return <Upload {...props}>
-  <Button icon={<UploadOutlined />}>点击上传JSON</Button>
-</Upload>
+    <Button icon={<UploadOutlined />}>点击上传JSON</Button>
+  </Upload>
 }
 
 const App: React.FC = () => {
-  const [tableData,setData] = useState(data)
+  const [tableData, setData] = useState(data)
   setTableData = setData
+  console.log('App-render');
+
   return <>
-  <Upfile/>
-  <Table rowKey="path" columns={columns} dataSource={tableData} />
+    <Upfile />
+    <Table rowKey="path" columns={columns} dataSource={tableData} />
   </>
-   
+
 };
 
 export default App;
